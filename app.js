006227278a2408ba99bacf345075e2ccc26c7764
Matej Drzik <matej.drzik@quanto.sk>
2016-02-02 14:29:54 +0100

    fixed swagger explorer path

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ module.exports = {
         // middlewares
         var bodyParser = require('body-parser');
 
-        app.use(express.static(__dirname + 'public'));
+        app.use(express.static(__dirname + '/public'));
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(bodyParser.json());
         app.use(require('./request_logger'));
